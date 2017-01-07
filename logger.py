f0bf011747e4d7dc670c4f7fde9ff58598a00303
Alessandro Marzialetti <setti93@gmail.com>
2017-01-07 21:20:40 +0100

    switched to nmap\n30s timeout\nDiff output

--- logger.py
+++ logger.py
@@ -2,15 +2,36 @@ import subprocess as sp
 import time
 import datetime
 
+def logDiff(f, previous, current):
+	f.write(str(time.time()) + ':\n')
+	#out
+	tmp = list(previous.difference(current))
+	for mac in tmp:
+		f.write("< " + mac + "\n")
+	#in
+	tmp = list(current.difference(previous))
+	for mac in tmp:
+		f.write("> " + mac + "\n")
+	f.flush()
+
+def getList(command):
+	res = sp.check_output(command).split('\n')[2:-4]
+	return [x.split()[2] for x in res if "MAC Address:" in x]
+
+command = ["nmap", "-sP", "192.168.1.0/24"]
 logfile = open("log" + str(datetime.datetime.now()).split('.')[0] + ".txt", 'w')
+macs = set(getList(command))
+timestep = 30
 
 try:
 	while True:
-		lst = sp.check_output(["arp-scan", "-l"])
-		logfile.write("time: " + str(time.time()) + '\n')
-		for line in lst.split('\n')[2:-4]:
-			logfile.write(line + '\n')
-		time.sleep(5)
+		start = int(time.time())
+		news = set(getList(command))
+		logDiff(logfile, macs, news)
+		macs = news
+		elapsed = int(time.time()) - start
+		if (elapsed < timestep):
+			time.sleep(timestep - elapsed)
 except KeyboardInterrupt:
 	print "Process stopped"
 	logfile.close()
